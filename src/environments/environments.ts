d63ef469aab2cde42f5419e3956bf8dd7929d204
AlejandroAn12 <negocios1219@gmail.com>
2023-12-15 00:08:50 -0500

    sdffa

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -4,5 +4,5 @@ export const environment = {
   // base_url: `http://192.168.0.102:3000/pay/v1`,
   base_url: `https://quipay-backend.onrender.com/pay/v1`,
   // base_url: `http://192.168.0.103:1219/pay/v1`,
-  // server_socket: `http://192.168.101.4:1219/socket.io/socket.io.js`,
+  server_socket: `http://192.168.101.4:1219/socket.io/socket.io.js`,
 };
